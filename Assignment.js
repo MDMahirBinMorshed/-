e018772efb67cbc6f4195222b834a84017702f3f
Md Mahir Bin Morshed <MDMahirBinMorshed>
2026-09-02 00:01:49 +0600

    first commit

---
+++ Assignment.js
@@ -0,0 +1,101 @@
+// Answer 1
+
+function describeValue(value) {
+    if (value) {
+        return typeof value + " | truthy";
+    } else {
+        return typeof value + " | falsy";
+    }
+}
+
+
+// Answer 2
+
+function getDayType(day) {
+    day = day.toLowerCase();
+
+    switch (day) {
+        case "friday":
+        case "saturday":
+            return "Weekend";
+
+        case "sunday":
+        case "monday":
+        case "tuesday":
+        case "wednesday":
+        case "thursday":
+            return "Working Day";
+
+        default:
+            return "Invalid Day";
+    }
+}
+
+
+// Answer 3
+
+function validateUsername(username) {
+    if (username.length < 4) {
+        return "Too Short";
+    } else if (username.includes(" ")) {
+        return "No Space Allowed";
+    } else if (username.toLowerCase().includes("admin")) {
+        return "Reserved Word";
+    } else {
+        return "Available";
+    }
+}
+
+
+// Answer 4
+
+function getCngFare(distance, isNight = false, waitingMinutes = 0) {
+    let fare = 50;
+
+    if (distance > 2) {
+        fare = fare + (distance - 2) * 15;
+    }
+
+    fare = fare + waitingMinutes * 2;
+
+    if (isNight) {
+        fare = fare * 1.20;
+    }
+
+    return fare;
+}
+
+
+// Answer 5
+
+const getChaseVerdict = (target, scored, ballsLeft) => {
+    let runsNeeded = target - scored;
+
+    if (runsNeeded <= 0) {
+        return "Won";
+    } else if (ballsLeft <= 0) {
+        return "Lost";
+    }
+
+    let requiredRate = (runsNeeded / ballsLeft) * 6;
+    let verdict;
+
+    if (requiredRate <= 6) {
+        verdict = "Comfortable";
+    } else if (requiredRate <= 12) {
+        verdict = "Tough";
+    } else {
+        verdict = "Almost Impossible";
+    }
+
+    return `Need ${runsNeeded} runs in ${ballsLeft} balls | ${verdict}`;
+};
+
+
+// Function Calls
+
+console.log(describeValue("hello"));
+console.log(getDayType("Friday"));
+console.log(validateUsername("rahim123"));
+console.log(getCngFare(5));
+console.log(getChaseVerdict(100, 90, 12));
